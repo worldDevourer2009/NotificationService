d7a6a6d1a27a5f165e614764eb4ccf5ecef4bf95
worldDevourer <danyterx123@gmail.com>
2025-07-03 16:36:42 +0200

    deleted unused shared folder

--- NotificationService.Shared/DTOs/EmailRequestDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskHandler.Application.DTOs;
-
-public class EmailRequestDTO
-{
-    public string? Subject { get; set; }
-    public string? Message { get; set; }
-    public string? HtmlMessage { get; set; }
-    public string[]? Attachments { get; set; }
-}
